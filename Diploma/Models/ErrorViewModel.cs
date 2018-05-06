b18a41c5c4eac47d3ea0885ca867121ad8184fb3
egorchernyshenko <egor.chernyshenko@gmail.com>
2018-05-06 14:59:57 +0300

    project setup

---
+++ Diploma/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Diploma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
